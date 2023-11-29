6db041b55da6a66730fc6a6d2ff171ba25c7470c
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-11-29 06:19:06 -0800

    print combo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
+for i in range(100):
+    if i < 99:
+        print(f'{i:02d}', end=', ')
     else:
-        print("{:02}".format(number), end=", ")
+        print(f'{i:02d}', end='')
