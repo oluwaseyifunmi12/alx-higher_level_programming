86043736c1ac06f54b4674f886f3c0845ca87495
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-12-05 20:00:22 -0800

    modifying a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        a = list(my_list)
+        a = my_list.copy()
         a[idx] = element
         return a
