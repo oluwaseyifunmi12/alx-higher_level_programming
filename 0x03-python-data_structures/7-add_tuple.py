a318ca65e6b24c449274509f1a53a3f994da6202
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-12-09 15:10:27 -0800

    tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,6 +3,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = tuple_a[:2] + (0, 0)
     tuple_d = tuple_b[:2] + (0, 0)
-    new = ()
-    new = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    new = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return new
