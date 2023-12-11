9a528b5720c84b31a37de4c178693e99193bf59e
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2023-12-11 03:50:36 -0800

    matrix squaring

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    my_matrix = []
+    for row in matrix:
+        result = list(map(lambda x : x ** 2, row))
+        my_matrix.append(result)
+    return my_matrix
