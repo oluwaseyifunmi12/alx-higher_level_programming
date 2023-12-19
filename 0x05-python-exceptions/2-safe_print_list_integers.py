742fb97e129c17a0098f7af59d76e5116c87ca4c
Daramola Victoria <victoria.mnpadm@gmail.com>
2023-12-18 20:11:32 -0800

    finally

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
-            
+
         except (ValueError, TypeError):
             continue
-    print('')
+    print()
     return (count)
