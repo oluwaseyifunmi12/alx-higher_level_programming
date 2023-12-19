34060c92a97f5bf81c72536586349452ed571aac
Daramola Victoria <victoria.mnpadm@gmail.com>
2023-12-18 17:58:05 -0800

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 def safe_print_list(my_list=[], x=0):
 
     count = 0
-    try:
-        for elements in range(x):
+    for elements in range(x):
+        try:
             print(my_list[elements], end=' ')
             count += 1
-    except IndexError:
-        pass
+        except IndexError:
+            break
     print()
     return (count)
