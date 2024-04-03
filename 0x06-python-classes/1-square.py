22ecfc09eddb341edfb62d2b8ac8e667df78d31d
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2024-04-03 11:47:23 -0700

    square class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-
 """Another square class with a size attribute"""
 
 
 class Square:
-
+    """square class attributes details
+    Attributes:
+        __size (int): gives the info on the size of a side
+    """
+    def __init__(self, size):
+        """initialization process
+        Args:
+            size (int): gives the info on the size of a side
+        Returns: none
+        """
+        self.__size = size
