a950804ba66082d3cc6dead8f1e333fdc6f5f049
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2024-04-10 06:41:13 -0700

    check

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,10 +8,11 @@ def add_integer(a, b=98):
             TypeError: if a or b is a non-integer or non-float.
         Both values should be typecated into int before addition
         Returns:
-            the value of addition of both numbers 
+            the value of addition of both numbers
     """
     if not all(isinstance(var, (int, float)) for var in (a, b)):
-        raise TypeError(" {} must be an integer".format(('a' if not isinstance(a, (int, float)) else 'b')))
+        raise TypeError(" {} must be an integer".format(
+            'a' if not isinstance(a, (int, float)) else 'b'))
 
     a = int(a)
     b = int(b)
