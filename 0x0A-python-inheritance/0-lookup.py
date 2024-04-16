351592206a5b422bdc5c09d7c7cc0a25923011fc
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2024-04-15 20:28:13 -0700

    methods available

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A function to methods and attributesshow """
+
+
+def lookup(obj):
+    """ This function just returns a list of available methods and attributes
+    """
+    return dir(obj)
