9e25299d0ad2fd67a0517273b92a771e36f2bf30
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2024-04-15 21:40:45 -0700

    empty class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A base geometry class"""
+
+
+class BaseGeometry:
+    """ An empty base geometry class"""
+    def area(self):
+        raise Exception ("area() is not implemented")
