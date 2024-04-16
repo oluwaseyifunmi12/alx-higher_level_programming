e7f0381d23ff96d5bb530982b35414960a2eeeaa
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2024-04-15 21:08:11 -0700

    methods available

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A function to sort list"""
+
+
+class MyList(list):
+    """A class that inherits from lists"""
+
+      def print_sorted(self):
+          """A function that prints sorted integers"""
+            print(sorted(self))
