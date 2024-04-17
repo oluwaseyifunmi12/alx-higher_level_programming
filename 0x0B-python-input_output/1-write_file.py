aa5c1139660cb3944cddf9b3ad180c8c765fb859
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2024-04-16 21:07:12 -0700

    openning a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function to writes to text files."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, mode = 'w', encoding="utf-8") as f:
+        char_count = f.write(text)
+        return char_count
