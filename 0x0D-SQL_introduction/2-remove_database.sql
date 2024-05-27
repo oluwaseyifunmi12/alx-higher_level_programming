976b1a62ce803c878e83aca2b91e851581da4e81
daramola oluwatoyin <victoria.mnpdm@gmail.com>
2024-05-27 01:37:41 +0100

    correction

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- remove a database from mysql server
-DROP DATABASE IF NOT EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- displays a full description of first_table
+SHOW CREATE TABLE first_table;
