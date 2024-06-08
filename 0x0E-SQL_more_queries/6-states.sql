06a1a2fbc88e96f4493065374f7685c7bb595127
daramola oluwatoyin <victoria.mnpdm@gmail.com>
2024-06-08 21:48:42 +0100

    increment

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+--  a script that creates the database and the table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
