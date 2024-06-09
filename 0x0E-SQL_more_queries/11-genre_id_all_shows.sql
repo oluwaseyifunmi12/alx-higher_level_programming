6ec654476a002009aafc4e2f07d910996ac83587
daramola oluwatoyin <victoria.mnpdm@gmail.com>
2024-06-09 15:33:26 +0100

    genre

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- a script that lists all shows contained in the database
 
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-LEFT JOIN tv_shows
+FROM tv_shows
+LEFT JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
