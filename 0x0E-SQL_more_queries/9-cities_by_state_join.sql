6d1131adc5c31c599bfb4b058be81720f9b1010a
Daramola Oluwatoyin <victoria.mnpadm@gmail.com>
2024-06-09 05:22:36 -0700

    comma

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- a script that lists all cities contained in the database
 
-SELECT cities.id cities.name states.name
+SELECT cities.id, cities.name, states.name
 FROM states
 JOIN cities
     ON states.id = cities.state_id
