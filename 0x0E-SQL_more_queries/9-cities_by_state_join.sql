ea19a36b64951f228bee77ed164282f43ab19c2b
daramola oluwatoyin <victoria.mnpdm@gmail.com>
2024-06-09 12:57:01 +0100

    ASC

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- a script that lists all cities contained in the database
+
+SELECT cities.id cities.name states.name
+FROM states
+JOIN cities
+    ON states.id = cities.state_id
+ORDER BY cities.id ASC;
