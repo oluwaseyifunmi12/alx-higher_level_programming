2eab007bfaf133d12bf532b9ee8a7f363100e3d4
daramola oluwatoyin <victoria.mnpdm@gmail.com>
2024-07-06 02:39:28 +0100

    ascii

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
 
-    data = urllib.parse.urlencode(values).encode('utf-8')
+    data = urllib.parse.urlencode(values).encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
