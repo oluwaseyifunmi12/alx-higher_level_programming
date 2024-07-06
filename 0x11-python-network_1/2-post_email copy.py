69b86ac340d8784bc64d18d480607f4eae86b35d
daramola oluwatoyin <victoria.mnpdm@gmail.com>
2024-07-06 03:36:09 +0100

    executable

--- 0x11-python-network_1/2-post_email copy.py
+++ 0x11-python-network_1/2-post_email copy.py
@@ -2,14 +2,16 @@
 """ A script sends a POST request to the passed URL
     with the email as a parameter
     and displays the body of the response
-    uing requests
 """
-import requests
+import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
     values = {"email": sys.argv[2]}
 
-    r = requests.post(url, data=values)
-    print(r.text)
+    data = urllib.parse.urlencode(values).encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
